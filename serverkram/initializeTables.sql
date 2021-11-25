5d00880b8ca5f4407fcd782c7d7bccc90129101e
MarHee <Christine.Heemann@haw-hamburg.de>
2021-11-25 16:57:52 +0100

    chattabelle mit testwerten eingefügt

--- serverkram/initializeTables.sql
+++ serverkram/initializeTables.sql
@@ -9,7 +9,7 @@ Usertabelle mit allen Werten für Registrierung und einem BehilfsBool für Playe
 */
 
 CREATE TABLE users (
-    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER PRIMARY KEY AUTO_INCREMENT,
     name TEXT NOT NULL,
     password TEXT NOT NULL,
     player INTEGER(1) /*SQLite hat kein Boolean*/
@@ -21,7 +21,7 @@ und einer Verlinkung für 4 Spieler, denke das sollte reichen
 */
 
 CREATE TABLE games (
-    gameID INTEGER PRIMARY KEY AUTOINCREMENT,   
+    gameID INTEGER PRIMARY KEY AUTO_INCREMENT,   
     roundsPlayed INTEGER NOT NULL, 
     active INTEGER(1),   
     player1 INTEGER,   
@@ -33,6 +33,20 @@ CREATE TABLE games (
     FOREIGN KEY(player3) REFERENCES users(userID), 
     FOREIGN KEY(player4) REFERENCES users(userID)    
     );
+
+    /*
+    TODO: Bilder & Texte hier speichern? wenn nicht wo/wie sonst?
+    */
+
+/*
+Chattabelle mit gesendeten Nachrichten, soll auf Startseite für User angezeigt werden
+*/
+
+CREATE TABLE chat (
+    msgID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    sentBy INTEGER NOT NULL,
+    msgText TEXT NOT NULL
+);
 
 /*Testwerte einfügen*/
 
@@ -46,3 +60,7 @@ INSERT INTO users (name, password, player) VALUES ("SpeckiSpectator", "lurking1s
 INSERT INTO games (roundsPlayed, active, player1, player2) VALUES (2, 1, 2, 5);
 INSERT INTO games (roundsPlayed, active, player1, player2, player3, player4) VALUES (6, 1, 2, 5, 4, 3);
 INSERT INTO games (roundsPlayed, active, player1, player2, player3, player4) VALUES (7, 0, 1, 3, 2, 5);
+
+INSERT INTO chat (sentBy, msgText) VALUES (3, "Hi guys!");
+INSERT INTO chat (sentBy, msgText) VALUES (1, "Hallo zusammen");
+INSERT INTO chat (sentBy, msgText) VALUES (4, "SpamSpamSpamSpam");
