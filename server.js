d45129327f9a9622513ee87294a9fb7d0c4c54b1
MarHee <Christine.Heemann@haw-hamburg.de>
2022-01-05 19:19:15 +0100

    Schnittstelle mit db steht, Anzeige Bilder funktioniert noch nicht

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/onupload', function(req, res) {
   });
   
   // Zeigt das Bild an
-app.get('/bildzeigen', function(req, res){
+    app.get('/bildzeigen', function(req, res){
     // console.log(testFilename);
     res.render("bildzeigen", {"filename": testFilename});
   });
@@ -105,7 +105,7 @@ app.get('/gameBeginn',function(req,res){
     res.sendFile(__dirname + '/views/gameFirst.html');
 });
 
-// Hier wird die Eingabe aus dem Server in die db gespeichert 
+/*// Hier wird die Eingabe aus dem Server in die db gespeichert 
 app.post("/Runde1", function(req,res){    
     const wasZeichnen= req.body.wasZeichnen1;
     res.render("gameZeichnen",{"wasZeichnen": wasZeichnen});
@@ -117,21 +117,17 @@ app.post("/Runde1", function(req,res){
         } else {
            res.sendFile(__dirname + "/views/game.html");
         }
-    }); */
-});
+    }); 
+});*/
 
-
-/*
 app.get('/game', function(req, res){
     res.sendFile(__dirname + '/views/game.html');
 });
-*/
 
-//Play-Button von Start
-// Testen ob User angemeldet ist?  --> Session gedöns?
+/*//Play-Button von Start
 app.post("/play", function(req,res){    
     res.redirect("/login");
-});
+});*/
 
 //Spielen-Button von Login
 app.post("/playAngemeldet", function(req,res){    
@@ -141,12 +137,13 @@ app.post("/playLaufend", function(req,res){
     res.sendFile(__dirname + "/views/game.html")
 });
 
+
 // Aufrug für gameSchreiben
-app.get('/schreiben',function(req,res){
+app.get('/gameSchreiben',function(req,res){
     res.sendFile(__dirname + '/views/gameSchreiben.html');
 })
 
-
+/*
 // POST von gameSchreiben
 app.post("/gameSchreiben", function(req,res){
     const antwortGezeichnet= req.body.zeichnenAntwort; //hier wird die Antwort des Users drin gespeichert
@@ -156,69 +153,34 @@ app.post("/gameSchreiben", function(req,res){
     //console.log(antwortGezeichnet);
 
 
-});
+});*/
 
 
 //Anzeige der aktiven Runde und Inhalt aktiver Runde
 app.post("/showRound", function(req,res){
-    global.param_gameID = req.body.input_gameID;
-    db.each(`SELECT roundsPlayed FROM games WHERE gameID  = ${param_gameID}`, (err, row) => {
+    param_gameID = req.body.input_gameID;
+    db.all(`SELECT * FROM games WHERE gameID  = ${param_gameID}`, (err, row) => {
         if (err) {
           res.send(err.message);
-        } else {
-            const getRound = function(callback){
-                db.each(`SELECT activeRound FROM games WHERE gameID  = ${param_gameID}`, (err, row) => {
-                    if (err) {
-                      res.send(err.message);
-                    } else {
-                        console.log(row.activeRound);
-                        var param_round = row.activeRound;
-                        callback(param_round); 
-                    }
-                });
-            
-            };
-            const activeRound = function(callback){
-                db.each(`SELECT activeRound FROM games WHERE gameID  = ${param_gameID}`, (err, row) => {
-                    if (err) {
-                        res.send(err.message);
-                    } else {
-                        var varGame = row.activeRound;
-                        callback(varGame);
-                    
-                    }
-                });
-            };
+        } else if (row.length > 0) {
+            // Result Row
+            const result = row[0];
+            // Values of retrieved columns
+            const gameID = result.gameID;   
+            const roundsPlayed = result.roundsPlayed;
+            const activeRound = result.activeRound;
 
-            const setRound = function(param_round, varGame, param_gameID, callback){
-                var game = param_gameID;
-                var round = param_round;
-                var content = varGame;
-                
-                callback([game,round,content]);
-                
-            }
-
-            setRound(getRound, activeRound, req.body.input_gameID);
-            
-            const use = function(game, round, content){
-                 console.log(game, round, content);
-            }
-            use(setRound);
-
-
-            if (round % 2 == 0){
-                //Runde gerade
-                res.render("rundeGerade", {"Bild": content}, {"Game": game}, {"Runde": round});
+            if (roundsPlayed % 2 == 0){
+                //Spiel einer UNgeraden Runde, also schreiben
+                res.sendFile("gameSchreiben", {"Game": gameID, "Runde": roundsPlayed +1, "filename": activeRound});
 
             } else {
-                //Runde ungerade
-                res.render("rundeUngerade", {"Text": content}, {"Game": game}, {"Runde": round});
-            }
-            /*global.varRounds = row.roundsPlayed;
-            console.log("Test" + varRounds);*/
-            
-        } 
+                //Spiel einer geraden Runde, also zeichnen
+                res.render("gameZeichnen", {"Game": gameID, "Runde": roundsPlayed, "wasZeichnen": activeRound});
+            }            
+        } else {
+            res.send("Fehler: Kein Game mit dieser ID gefunden.");
+        }
     });
 });
 
@@ -236,10 +198,10 @@ app.post("/Runde1", function(req,res){
     });
 });
 
-app.post("/Gerade", function(req,res){
+app.post("/zeichnenFertig", function(req,res){
     const param_gameID = req.body.gameID;
-    const param_round = req.body.round;
-    const param_newRound = param_round + 1;
+    const param_round = req.body.round-1;
+    const param_newRound = req.body.round;
     const param_img = req.body.img; //variable "testFilename"
 
     db.run( `INSERT INTO games WHERE gameID ='${param_gameID}' 
@@ -248,18 +210,19 @@ app.post("/Gerade", function(req,res){
         if (err){
             res.send(err.message)
         } else {
-            //res.sendFile(__dirname + "/views/game.html");
+            //
             console.log(`uploaded ${param_img} to round ${param_round} and set RP to ${param_newRound}`);
-            res.send(`uploaded ${param_img} to round ${param_round} and set RP to ${param_newRound}`);
+            res.sendFile(__dirname + "/views/game.html");
+            //res.send(`uploaded ${param_img} to round ${param_round} and set RP to ${param_newRound}`);
         }
     });
 }); 
 
-app.post("/Ungerade", function(req,res){
+app.post("/gameSchreiben", function(req,res){
     const param_gameID = req.body.gameID;
-    const param_round = req.body.round;
-    const param_newRound = param_round + 1;
-    const param_text = req.body.text; //variable "wasZeichnen"
+    const param_round = req.body.round-1;
+    const param_newRound = req.body.round;
+    const param_text = req.body.zeichnenAntwort; //variable "wasZeichnen"
 
     db.run( `INSERT INTO games WHERE gameID ='${param_gameID}' 
     (roundsPlayed, round${param_round}, activeRound) 
@@ -267,9 +230,10 @@ app.post("/Ungerade", function(req,res){
         if (err){
             res.send(err.message)
         } else {
-            //res.sendFile(__dirname + "/views/game.html");
+            //
             console.log(`uploaded ${param_text} to round ${param_round} and set RP to ${param_newRound}`);
-            res.send(`uploaded ${param_text} to round ${param_round} and set RP to ${param_newRound}`);
+            //res.send(`uploaded ${param_text} to round ${param_round} and set RP to ${param_newRound}`);
+            res.sendFile(__dirname + "/views/game.html");
         }
     });
 });   
@@ -316,6 +280,7 @@ app.post('/userRegister', function(req, res){
 
     if (param_passwordInput.match(/^[0-9]+$/) || param_passwordInput.length < 6){
         res.send("Dein Passwort ist etwas zu einfach")
+
     } else if (param_passwordRepeat != param_passwordInput){
         res.send("Deine Eingaben passen nicht zusammen")
     } else {
@@ -323,7 +288,8 @@ app.post('/userRegister', function(req, res){
             if (err){
                 res.send(err.message)
             } else {
-                res.render("loginErfolgreich", {"benutzername": param_usernameInput});            }
+                res.render("loginErfolgreich", {"benutzername": param_usernameInput});
+            }
         });   
     } 
 });
