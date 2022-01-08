64a067b55c41124ca1a232233d8b474590d8346b
WhiskyBoi <mert.ayhan@haw-hamburg.de>
2022-01-08 21:48:28 +0100

    Templates Platzierung im Game, Buttons CSS
    Templates Zentral Vertikal, fehlende Buttons CSS ergänzt

--- scripts/drawOnCanvas.js
+++ scripts/drawOnCanvas.js
@@ -11,7 +11,7 @@ function setAttributes(element, attributes) {
     }
 }
 
-setAttributes(canvas, {id: "canvas", width: "900px", height: "600px;", style: "border:3px #94cf03 dotted;"})
+setAttributes(canvas, {id: "canvas", width: "1000px;", height: "420px;", style: "border: 4px #94cf03 dotted;"})
 
 // get canvas 2D context and set him correct size
 let ctx = canvas.getContext('2d');
